6f234d99dd8b0e59b9166c761372c267d4d83b48
Miguel Munoz E <miguelestradam12@gmail.com>
2024-10-22 16:47:25 -0500

    server: validates an conexion mongo

--- Backend Development and API creation/config/keys.js
+++ Backend Development and API creation/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     "mongodb+srv://miguelestradam12:R3vH9EJkcamU9Pe3@cluster0.ro08k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  secretOrKey: "12345rt",
 };
